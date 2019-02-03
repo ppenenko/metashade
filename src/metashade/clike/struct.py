4a7a10a7a8486b962a125ed18ced2f93af399abd
Pavlo Penenko <pavlo.penenko@gmail.com>
2019-02-02 22:56:48 -0500

    Struct members are added as members of the Python object. Raising an exception if a struct member is bound (assigned by reference) after construction.

--- src/metashade/clike/struct.py
+++ src/metashade/clike/struct.py
@@ -19,23 +19,30 @@ class Struct(metashade.clike.data_types.BaseType):
     def __init__(self, struct_def):        
         super(Struct, self).__init__()
         
-        self._struct_def = struct_def
-        self._members = {name : data_type() \
-                 for name, data_type in struct_def._member_defs.iteritems()}        
+        self._struct_def = struct_def        
+        for name, data_type in struct_def._member_defs.iteritems():
+            setattr(self, name, data_type())
+        
+        self._constructed = True        
         
     def define(self, sh, identifier):
         super(Struct, self).define(sh, identifier)
         
-        for member_name, member in self._members.iteritems():
-            nested_name='{struct_name}.{member_name}'.format(
-                struct_name=identifier, member_name=member_name)
-            member._bind(sh, nested_name, allow_defaults=False)            
+        for member_name, member in vars(self).iteritems():
+            if not member_name.startswith('_'):
+                nested_name='{struct_name}.{member_name}'.format(
+                    struct_name=identifier, member_name=member_name)
+                member._bind(sh, nested_name, allow_defaults=False)           
         
     def get_target_type_name(self):
         return self._struct_def._identifier
     
-    def __getattr__(self, name):
-        return self._members[name]
+    def __setattr__(self, name, value):
+        if not name.startswith('_') and hasattr(self, '_constructed'):
+            raise RuntimeError(
+                "Metashade struct member can't be bound after construction")
+            
+        super(Struct, self).__setattr__(name, value)
 
 class StructDef(object):
     def __init__(self, **kwargs):
