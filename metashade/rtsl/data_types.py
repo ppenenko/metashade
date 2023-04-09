0ebc9f9cf40fa4f2338283c939d4a98486e0b573
Pavlo Penenko <pavlo.penenko@gmail.com>
2023-04-09 19:28:03 -0400

    Fixed matrix product dimensions test

--- metashade/rtsl/data_types.py
+++ metashade/rtsl/data_types.py
@@ -167,7 +167,7 @@ class _RawMatrix(clike.ArithmeticType):
         return getattr(sys.modules[cls.__module__], type_name)
 
     def _check_mul(self, vector):
-        if self.__class__._dims[1] != vector.__class__._dim:
+        if self.__class__._dims[0] != vector.__class__._dim:
             raise ArithmeticError(
                 'The number of columns in the matrix must be equal to the size'
                 'of the vector'
