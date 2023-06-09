9ca26bc3aa82b7ee62be75863799855b0d76b719
Pavlo Penenko <pavlo.penenko@gmail.com>
2023-06-09 09:53:57 -0400

    Basic test for vector initialization

---
+++ tests/out/test_vector_init.hlsl
@@ -0,0 +1,12 @@
+float vector_init(float3 rgb, float4 rgba)
+{
+	float f = 1;
+	float2 f2 = f.xx;
+	f2 = 2.xx;
+	f2 = 0.xx;
+	float3 f3 = f.xxx;
+	f3 = 0.xxx;
+	f3 = 2.xxx;
+	return f;
+}
+
---
+++ tests/test_vector_init.py
@@ -0,0 +1,41 @@
+# Copyright 2023 Pavlo Penenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import _base
+from metashade.hlsl.sm6 import ps_6_0
+
+class TestVectorInit(_base.Base):
+    def test_vector_init(self):
+        hlsl_path = self._get_hlsl_path('test_vector_init')
+        with self._open_file(hlsl_path) as ps_file:
+            sh = ps_6_0.Generator(ps_file)
+
+            with sh.function('vector_init', sh.Float)(
+                rgb = sh.RgbF, rgba = sh.RgbaF
+            ):
+                sh.f = sh.Float(1)
+
+                sh.f2 = sh.Float2(sh.f)
+                sh.f2 = sh.Float2(sh.Float(2))
+                sh.f2 = sh.Float2(0)
+                #sh.f2 = sh.Float2((0, 1))
+
+                sh.f3 = sh.Float3(sh.f)
+                sh.f3 = sh.Float3(0)
+                sh.f3 = sh.Float3(sh.Float(2))
+                #sh.f3 = sh.Float3((0, 1, 2))
+
+                sh.return_(sh.f)
+
+        self._compile(hlsl_path, as_lib = True)
