53276deb8a04327268ec4c72881a78cb56d418e0
Pavlo Penenko <pavlo.penenko@gmail.com>
2023-06-16 13:26:07 -0400

    Simple test for samplers

---
+++ tests/out/test_combined_samplers.hlsl
@@ -0,0 +1,23 @@
+Texture2D colorTexture0 : register(t0);
+SamplerState colorSampler0 : register(s1);
+
+Texture2D colorTexture1 : register(t1);
+SamplerState colorSampler1 : register(s0);
+
+struct VsOut
+{
+	float2 uv0 : TEXCOORD;
+};
+
+struct PsOut
+{
+	float4 color : SV_TARGET;
+};
+
+PsOut psMain(VsOut psIn)
+{
+	PsOut psOut;
+	psOut.color = colorTexture1.Sample(colorSampler1, psIn.uv0);
+	return psOut;
+}
+
---
+++ tests/test_samplers.py
@@ -0,0 +1,45 @@
+# Copyright 2023 Pavlo Penenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import _base
+from metashade.hlsl.sm6 import ps_6_0
+
+class TestSamplers(_base.Base):
+    def test_combined_samplers(self):
+        hlsl_path = self._get_hlsl_path('test_combined_samplers')
+        with self._open_file(hlsl_path) as ps_file:
+            sh = ps_6_0.Generator(ps_file)
+
+            sh.combined_sampler_2d(
+                texture_name = 'colorTexture0', texture_register = 0,
+                sampler_name = 'colorSampler0', sampler_register = 1
+            )
+            sh.combined_sampler_2d(
+                texture_name = 'colorTexture1', texture_register = 1,
+                sampler_name = 'colorSampler1', sampler_register = 0,
+                texel_type = sh.RgbaF
+            )
+
+            with sh.vs_output('VsOut') as VsOut:
+                VsOut.texCoord('uv0', sh.Point2f)
+
+            with sh.ps_output('PsOut') as PsOut:
+                PsOut.SV_Target('color', sh.Float4)
+
+            with sh.main(self._entry_point_name, sh.PsOut)(psIn = sh.VsOut):
+                sh.psOut = sh.PsOut()
+                sh.psOut.color = sh.colorSampler1(sh.psIn.uv0)
+                sh.return_(sh.psOut)
+
+        self._compile(hlsl_path, as_lib = False)
