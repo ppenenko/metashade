9513137681eb7287671db685c64337b76c1fb57a
Pavlo Penenko <pavlo.penenko@gmail.com>
2024-02-03 18:50:59 -0500

    A hypothetical example of generating polymorphically for HLSL and GLSL

--- examples/create_generator.py
+++ examples/create_generator.py
@@ -1,5 +1,15 @@
-from metashade.hlsl.sm6 import ps_6_0
+from metashade.hlsl.sm6 import ps_6_0 as hlsl_ps
+from metashade.glsl.v460 import frag as glsl_fs # hypothetical
 
-with open('ps.hlsl', 'w') as ps_file:
-    sh = ps_6_0.Generator(ps_file)
-    # Generate some code with `sh`
+def generate(sh):
+    # Polymorphic shader code for multiple targets
+    pass
+
+with open('ps.hlsl', 'w') as hlsl_file:
+    sh = hlsl_ps.Generator(hlsl_file)
+    generate(sh)
+
+with open('fs.glsl', 'w') as glsl_file:
+    sh = glsl_fs.Generator(glsl_file)
+    generate(sh)
+
