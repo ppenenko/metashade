d70710f5ae51499bd4bd7ed965d8bb316f88f16e
Pavlo Penenko <pavlo.penenko@gmail.com>
2024-02-16 18:01:53 -0500

    More example refinement

--- examples/misc.py
+++ examples/misc.py
@@ -7,22 +7,29 @@ from metashade.hlsl.sm6 import ps_6_0 as hlsl_ps
 with open('misc.hlsl', 'w') as hlsl_file:
     sh = hlsl_ps.Generator(hlsl_file)
 
-    with sh.function('foo', sh.Float)(N = sh.Vector3f, L = sh.Vector3f):
+    with sh.function('foo', sh.Float)(
+        N = sh.Vector3f, L = sh.Vector3f
+    ):
         sh // 'Create a float variable with the value of pi'
         sh.x = sh.Float(math.pi)
 
-        sh // 'Swizzling and write masking'
         sh.rgba = sh.RgbaF(rgb = (0, 1, 0), a = 0)
 
-        sh // "The variable type is deduced below, a-la `auto` in C++"
+        sh // 'Swizzling - the destination type is deduced'
+        sh // "a-la `auto` in C++"
         sh.color = sh.rgba.rgb
+
+        sh // 'Write masking'
         sh.color.r = 1
 
-        sh // 'Some intrinsics'
+        sh // 'Intrinsics example'
         sh.N = sh.N.normalize()
 
-        sh // 'Dot product'
+        sh // 'Dot product == Python 3 matmul'
+        sh // '(a.k.a. "walrus") operator'
         sh.NdotL = sh.N @ sh.L
+
+        sh.xyz = sh.rgba.xyz    # Exception: `RgbaF` has no attribute `xyz`
 
         sh.return_(sh.NdotL)
 
@@ -30,8 +37,3 @@ with open('misc.hlsl', 'w') as hlsl_file:
         # texture-sampler combination and sampling
         # Comments
         # build abstractions on top, e.g. glTF textures in my demo
-
-with open('fail.hlsl', 'w') as hlsl_file:
-    sh = hlsl_ps.Generator(hlsl_file)
-    with sh.function('foo')():
-        sh.xyz = sh.rgba.xyz    # Exception: `RgbaF` has no attribute `xyz`
